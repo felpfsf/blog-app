5311882d4afa81f18a358e6b774f7cd92df80722
Felipe F <feferreira.sf@gmail.com>
2023-01-09 19:17:26 -0300

    feat: timeline with tweets exhibit

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,10 +1,15 @@
+import { api } from "../utils/api";
 import CreateTweet from "./CreateTweet";
 
 function Timeline() {
+  const { data } = api.tweet.timeline.useQuery({
+    limit: 1,
+  });
   return (
     <div>
       <h1>Tweet Something</h1>
       <CreateTweet />
+      {JSON.stringify(data)}
     </div>
   );
 }
