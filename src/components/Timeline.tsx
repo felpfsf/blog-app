45359d042e8c65b80e9397095e49c6be7483880b
Felipe F <feferreira.sf@gmail.com>
2023-01-09 20:11:34 -0300

    feat: infinite scroll

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,19 +1,35 @@
+import { useEffect } from "react";
+import useScrollPosition from "../hooks/useScrollPosition";
 import { api } from "../utils/api";
 import CreateTweet from "./CreateTweet";
 import Tweet from "./Tweet";
 
+const LIMIT = 10;
+
 function Timeline() {
-  const { data } = api.tweet.timeline.useQuery({
-    limit: 1,
-  });
+  const { data, hasNextPage, fetchNextPage, isFetching } =
+    api.tweet.timeline.useInfiniteQuery(
+      {
+        limit: LIMIT,
+      },
+      { getNextPageParam: (lastPage) => lastPage.nextCursor }
+    );
+  const tweets = data?.pages.flatMap((page) => page.tweets) ?? [];
+
+  const scrollPosition = useScrollPosition();
+
+  useEffect(() => {
+    if (scrollPosition > 90 && hasNextPage && !isFetching) {
+      void fetchNextPage();
+    }
+  }, [scrollPosition, hasNextPage, isFetching, fetchNextPage]);
   return (
     <div>
       <h1>Tweet Something</h1>
       <CreateTweet />
-      {data?.tweets.map((tweet) => (
+      {tweets.map((tweet) => (
         <Tweet key={tweet.id} tweet={tweet} />
       ))}
-      {JSON.stringify(data)}
     </div>
   );
 }
