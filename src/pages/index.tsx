04c8c298b1bdb6dc2164d92240923fafe6d3e7c6
Felipe F <feferreira.sf@gmail.com>
2023-01-09 21:50:23 -0300

    refactor: removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "../utils/api";
 import Timeline from "../components/Timeline";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -17,7 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Timeline />
+        <Timeline where={undefined} />
       </div>
     </>
   );
